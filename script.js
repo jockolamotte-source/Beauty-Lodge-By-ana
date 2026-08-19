3749128eae1396ca2ba01d239479cc06ad7573cb
jockolamotte-source <jocko.lamotte@gmail.com>
2026-08-18 22:10:34 -0400

    Fix image filename case for natural set

--- script.js
+++ script.js
@@ -526,7 +526,7 @@ const LASH_STYLE_OPTIONS = {
   natural: {
     tabId: "lash-choice-natural",
     title: "Natural Full Set",
-    image: "assets/newnatural.jpg",
+    image: "assets/NewNatural.jpg",
     imageAlt: "Natural Full Set lash result",
     description:
       "A soft, polished extension look that adds length and definition without appearing overly dramatic.",
